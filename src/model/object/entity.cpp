46a4557e77dc6da80102276213d4e75ad945d600
Xerus <27jf@web.de>
2019-10-01 17:57:50 +0200

    refactor(model): rename object to entity and entity to individual

---
+++ src/model/object/entity.cpp
@@ -0,0 +1,13 @@
+//
+// Created by aaron on 26/09/2019.
+//
+
+#include "entity.h"
+
+Entity::Entity(Point position) : position{position} {}
+Entity::Entity(int x, int y) : position{x, y} {}
+Entity::~Entity() = default;
+
+Point Entity::getPosition() {return position;}
+void Entity::setPosition(Point p) { position = p;}
+void Entity::setPosition(int x, int y) { position = Point(x, y);}
