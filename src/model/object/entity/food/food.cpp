dc8f7929678d67f2105be19ac723a81266ecce31
Aaron Alef <aaron.alef@code.berlin>
2019-10-02 12:27:01 +0200

    fix(model): apply a fix for the change in commit 2c621c1 (in food)

--- src/model/object/entity/food/food.cpp
+++ src/model/object/entity/food/food.cpp
@@ -7,6 +7,10 @@
 Food::Food(int x, int y, int value) : Entity(x, y), value{value} {checkValue();}
 Food::Food(Point position, int value) : Entity(position), value{value} {checkValue();}
 
+Shape Food::getHitBox() {
+    return Shape();
+}
+
 void Food::checkValue() {
     if (value <= 0) throw std::range_error("Food can't have a negative value");
 }
