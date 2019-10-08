e0f195537b98f1514a10c86966f4aa9b3bbf5bbd
AtomToast <reg-2@t-online.de>
2019-10-08 19:06:19 +0200

    feat: create terminalview.h

--- src/view/terminalview.h
+++ src/view/terminalview.h
@@ -7,6 +7,9 @@
 class TerminalView {
 public:
     void render(World world);
+    std::string createTable(World world);
+    std::string stringifyFood(Food food);
+    std::string stringifyEntity(Entity & entity);
 };
 
 #endif // EVE_TERMINALVIEW_H
