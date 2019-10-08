c8edbbb03f22122e38685400096fccd67340b8f1
AtomToast <reg-2@t-online.de>
2019-10-08 16:57:29 +0200

    feat(view): add terminalview

---
+++ src/view/terminalview.h
@@ -0,0 +1,12 @@
+
+#ifndef EVE_TERMINALVIEW_H
+#define EVE_TERMINALVIEW_H
+
+#include "../model/world/world.h"
+
+class TerminalView {
+public:
+    void render(World world);
+};
+
+#endif // EVE_TERMINALVIEW_H
