578219f602f5924a062e462a4f174c6e764122bb
szudemj <simon.joedicke@code.berlin>
2019-10-14 14:06:24 +0200

    fix(food): revert chamges
    Uncommenting a line of code was accidentally commited

--- src/model/object/entity/food/food.cpp
+++ src/model/object/entity/food/food.cpp
@@ -5,6 +5,6 @@
 #include "food.h"
 
 Food::Food(int x, int y, int energy) : Entity(x, y, energy) {}
-//Food::Food(Point position, int energy) : Entity(position.x, position.y, energy) {}
+Food::Food(Point position, int energy) : Entity(position.x, position.y, energy) {}
 
 Food::Entity::~Entity() = default;
