492015447b57118f8d11a36e5643a1c8c9a28ba4
AtomToast <reg-2@t-online.de>
2019-10-22 16:41:30 +0200

    test: add tests for stringifyIndividual and createTable

--- test/view/terminalview.cpp
+++ test/view/terminalview.cpp
@@ -1,4 +1,5 @@
 #include "../../src/view/terminalview.h"
+#include "../../src/model/object/entity/individual/fred/fred.h"
 #include <catch2/catch.hpp>
 #include <exception>
 
@@ -11,36 +12,62 @@ TEST_CASE("A string table can be created"){
         "—————————————————————————————————————————\n"
         "X:  500 | Y:  689 | Energy:  10\n"
         "X: 1000 | Y: 1000 | Energy:  10\n"
-        "X:   43 | Y:  978 | Energy:  20\n"
+        "X:    0 | Y:    0 | Energy:   1\n"
         "—————————————————————————————————————————\n"
         "Individuals:\n"
         "—————————————————————————————————————————\n"
-        "ID:  23 | X:  123 | Y:  321 | Energy: 100\n"
-        "ID:  24 | X:  138 | Y:  153 | Energy:  70\n"
-        "ID:  69 | X:  420 | Y:  420 | Energy:   5\n";
+        "ID:   0 | X:  123 | Y:  321 | Energy: 100\n"
+        "ID:   1 | X:  138 | Y:  153 | Energy:  70\n"
+        "ID:   2 | X:  420 | Y:  420 | Energy:   5\n";
 
     World world = World(1000, 1000, 40);
-    SECTION(""){}
+    Fred * fred1 = new Fred(123, 321, 100);
+    Fred * fred2 = new Fred(138, 153, 70);
+    Fred * fred3 = new Fred(420, 420, 5);
+
+    Food * food1 = new Food(500, 689, 10);
+    Food * food2 = new Food(1000, 1000, 100);
+    Food * food3 = new Food(0, 0, 1);
+
+    world.addIndividual(fred1);
+    world.addIndividual(fred2);
+    world.addIndividual(fred3);
+
+    world.addFood(food1);
+    world.addFood(food2);
+    world.addFood(food3);
+
 
     // Food tests
-    Food food1 = Food(500, 689, 10);
     SECTION("stringifyFood can convert a Food object into a string") {
         REQUIRE(
             terminalView.stringifyFood(food1) == "X:  500 | Y:  689 | Energy:  10\n"
         );
     }
 
-    Food food2 = Food(1000, 1000, 100);
     SECTION("stringifyFood correctly formats food at max values") {
         REQUIRE(
             terminalView.stringifyFood(food2) == "X: 1000 | Y: 1000 | Energy: 100\n"
         );
     }
 
-    Food food3 = Food(0, 0, 1);
     SECTION("stringifyFood correctly formats food at minimum values"){
         REQUIRE(
             terminalView.stringifyFood(food3) == "X:    0 | Y:    0 | Energy:   1\n"
+        );
+    }
+
+    // Individual tests
+    SECTION("stringifyIndividual can convert an individual object into a string") {
+        REQUIRE(
+            terminalView.stringifyIndividual(fred1) == "ID:   0 | X:  123 | Y:  321 | Energy: 100\n"
+        );
+    }
+
+    // createTable tests
+    SECTION("createTable correctly formats the whole table"){
+        REQUIRE(
+            terminalView.createTable(world) == table;
         );
     }
 }
