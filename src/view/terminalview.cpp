f5d41db04cbe8a5aac5d1e699ba9d37e94b9770b
AtomToast <reg-2@t-online.de>
2019-10-22 13:02:15 +0200

    fix: change value to energy

--- src/view/terminalview.cpp
+++ src/view/terminalview.cpp
@@ -4,7 +4,7 @@ std::string TerminalView::formatNumberSpacing(int number, int maxLen) {
     std::string formattedString;
     formattedString = std::to_string(number);
     while (formattedString.length() < maxLen) {
-        formattedString = std::string::space(" ").std::string::append(formattedString);
+        formattedString = std::string(" ").std::string::append(formattedString);
     }
     return formattedString;
 };
@@ -12,19 +12,19 @@ std::string TerminalView::formatNumberSpacing(int number, int maxLen) {
 std::string TerminalView::stringifyFood(Food food) {
     int xPos = food.getPosition().x;
     int yPos = food.getPosition().y;
-    int value = food.getValue();
+    int energy = food.getEnergy();
 
     std::string formattedXPos;
     std::string formattedYPos;
-    std::string formattedValue;
+    std::string formattedEnergy;
 
     formattedXPos = formatNumberSpacing(xPos, 4);
     formattedYPos = formatNumberSpacing(yPos, 4);
-    formattedValue = formatNumberSpacing(value, 3);
+    formattedEnergy = formatNumberSpacing(energy, 3);
 
     std::string outputString;
     outputString += "X: " + formattedXPos + " | ";
     outputString += "Y: " + formattedYPos + " | ";
-    outputString += "Value: " + formattedValue + "\n";
+    outputString += "Energy: " + formattedEnergy + "\n";
     return outputString;
 };
