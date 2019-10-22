cdf5b6a5fb0f2902cce2dba43be0492b2709d150
szudemj <simon.joedicke@code.berlin>
2019-10-22 16:05:10 +0200

    feat(model): implement empty method act

--- src/model/object/entity/individual/fred/fred.cpp
+++ src/model/object/entity/individual/fred/fred.cpp
@@ -3,3 +3,5 @@
 Fred::Fred(int x, int y, int energy) : Individual(x, y, energy) {}
 
 Fred::Individual::~Individual() = default;
+
+void Fred::act() {}
--- src/model/object/entity/individual/fred/fred.h
+++ src/model/object/entity/individual/fred/fred.h
@@ -6,6 +6,8 @@
 class Fred : public Individual {
 public:
     Fred(int x, int y, int energy);
+
+    void act();
 };
 
 #endif //EVE_FRED_H
