1c0c45c41122a96d119713d76c254723ca5f4111
szudemj <simon.joedicke@code.berlin>
2019-10-22 15:43:08 +0200

    test(model): add basic test case to fred

--- test/model/object/entity/individual/fred/fred.cpp
+++ test/model/object/entity/individual/fred/fred.cpp
@@ -6,9 +6,11 @@
 
 SCENARIO("Fred can be created and initialized") {
     GIVEN("An individual of class Fred with basic properties is instantiated") {
-        WHEN("The individual is requested to act on a value") {
-            THEN("The individual should return the value") {
+        Fred fred = Fred(3, 5, 60);
 
+        WHEN("The individual is requested to act") {
+            THEN("The method should not throw an error") {
+                REQUIRE_NOTHROW(fred.act());
             }
         }
     }
