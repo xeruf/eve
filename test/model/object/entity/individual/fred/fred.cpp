af4a12bbd6df205a6d117e54c15cabfd5a9799c4
szudemj <simon.joedicke@code.berlin>
2019-10-22 15:26:23 +0200

    test(model): initial BDD structure for class Fred

---
+++ test/model/object/entity/individual/fred/fred.cpp
@@ -0,0 +1,15 @@
+#include <catch2/catch.hpp>
+#include <exception>
+#include <cmath>
+
+#include "../../../../../../src/model/object/entity/individual/fred/fred.cpp"
+
+SCENARIO("Fred can be created and initialized") {
+    GIVEN("An individual of class Fred with basic properties is instantiated") {
+        WHEN("The individual is requested to act on a value") {
+            THEN("The individual should return the value") {
+
+            }
+        }
+    }
+}
