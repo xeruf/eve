8336ced18e15da6b5055cf7d7d9494826860caf2
Aaron Alef <aaron.alef@code.berlin>
2019-10-25 17:38:03 +0200

    feat(model): add more operators for the point struct

--- src/model/model.h
+++ src/model/model.h
@@ -9,7 +9,9 @@ struct Point {
     Point(int x, int y);
 };
 
-bool operator == (const Point &, const Point &);
-double operator - (const Point &, const Point &);
+bool operator == (const Point & a, const Point & b);
+double operator / (const Point & a, const Point & b);
+Point operator - (const Point & a, const Point & b);
+Point operator + (const Point & a, const Point & b);
 
 #endif //EVE_MODEL_H
