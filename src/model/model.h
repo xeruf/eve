c7248fdacfc4a6b3ab757781794ba4ea1e37d9ac
Aaron Alef <aaron.alef@code.berlin>
2019-10-28 22:04:02 +0100

    refactor(model): move operator overloading for Points into its struct

--- src/model/model.h
+++ src/model/model.h
@@ -7,11 +7,12 @@ struct Point {
     int x;
     int y;
     Point(int x, int y);
+
+    bool operator == (const Point &);
+    double operator / (const Point & a);
+    Point operator - (const Point & a);
+    Point operator + (const Point & a);
 };
 
-bool operator == (const Point & a, const Point & b);
-double operator / (const Point & a, const Point & b);
-Point operator - (const Point & a, const Point & b);
-Point operator + (const Point & a, const Point & b);
 
 #endif //EVE_MODEL_H
