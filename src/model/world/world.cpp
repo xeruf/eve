b12eca4637a409ed9dbaff5fc7bdebc2b61ecc79
Aaron Alef <aaron.alef@code.berlin>
2019-10-29 13:35:12 +0100

    fix(world): replace the old distance operator '-' with the new '/' (#27)

--- src/model/world/world.cpp
+++ src/model/world/world.cpp
@@ -50,7 +50,7 @@ std::vector<Object *> * World::getObjectsAround(const Point & position, double r
     auto visibles = new std::vector<Object *>();
     for (auto objects : objectLists) {
         for (auto & object : * objects) {
-            if (std::abs(object->getPosition() - position) < radius) visibles->push_back(std::move(object));
+            if (std::abs(object->getPosition() / position) < radius) visibles->push_back(std::move(object));
         }
     }
     return visibles;
