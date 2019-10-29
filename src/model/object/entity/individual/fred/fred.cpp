4211bdeaf105e11d69d26873568dc621f3357e27
Aaron Alef <aaron.alef@code.berlin>
2019-10-29 13:33:51 +0100

    fix(object): replace old Action creation mechanic in Fred

--- src/model/object/entity/individual/fred/fred.cpp
+++ src/model/object/entity/individual/fred/fred.cpp
@@ -5,5 +5,5 @@ Fred::Fred(int x, int y, int energy) : Individual(x, y, energy) {}
 Fred::Individual::~Individual() = default;
 
 Action * Fred::act(std::vector<Object *> * visibles) {
-    return new Action((action) (visibles->size() % SIZE));
+    return new Sleep();
 }
