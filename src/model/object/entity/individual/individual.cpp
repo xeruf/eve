d6d3b16aacb8ba6124d9d6761c17e49dcc37cb07
Aaron Alef <aaron.alef@code.berlin>
2019-11-05 15:38:16 +0100

    fix(object): remove duplicate destructor

--- src/model/object/entity/individual/individual.cpp
+++ src/model/object/entity/individual/individual.cpp
@@ -9,8 +9,6 @@ Individual::Individual(double x, double y, double a, double energy) :
 
 Individual::~Individual() = default;
 
-Individual::~Individual() = default;
-
 long Individual::getID() {return ID;}
 
 double Individual::getDirection() {return direction;}
