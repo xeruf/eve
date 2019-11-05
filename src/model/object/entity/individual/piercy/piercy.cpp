924898034ab49930faca3fd678765b08b3f8e999
Aaron Alef <aaron.alef@code.berlin>
2019-11-05 13:21:34 +0100

    feat(object): replace the amount to flush in by a constant

--- src/model/object/entity/individual/piercy/piercy.cpp
+++ src/model/object/entity/individual/piercy/piercy.cpp
@@ -1,10 +1,11 @@
+#include <textstyle.h>
 #include "piercy.h"
 
 Piercy::Piercy(double x, double y, double energy) : Individual(x, y, energy) {}
 
 Action * Piercy::act(std::vector<Object *> * visibles) {
     while (true) {
-        std::cin.ignore(10000, '\n');
+        std::cin.ignore(FLUSH_AMOUNT, '\n');
         switch (std::getchar()) {
             case 'e':
                 return new Eat();
