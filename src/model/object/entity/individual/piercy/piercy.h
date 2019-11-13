3ef708f8e47613adff762366d01cd6d2a7841a7f
Aaron Alef <aaron.alef@code.berlin>
2019-11-13 14:23:36 +0100

    style(object): mark Piercy::act as override

--- src/model/object/entity/individual/piercy/piercy.h
+++ src/model/object/entity/individual/piercy/piercy.h
@@ -14,7 +14,7 @@ class Piercy : public Individual {
 public:
     Piercy(double x, double y, double a, double energy);
 
-    Action * act(std::vector<Object *> * visibles);
+    Action * act(std::vector<Object *> * visibles) override;
 };
 
 #endif //EVE_PIERCY_H
