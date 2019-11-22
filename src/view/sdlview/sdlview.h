3dbaa44f6a4025c632f81288d4925069193002a5
AtomToast <reg-2@t-online.de>
2019-11-22 18:49:23 +0100

    feat(view): create a visualisation of the individuals in the world

---
+++ src/view/sdlview/sdlview.h
@@ -0,0 +1,19 @@
+#ifndef EVE_SDLVIEW_H
+#define EVE_SDLVIEW_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
+
+#include "../../model/world/world.h"
+
+class SDLView {
+private:
+    void setupsdl(const World & world);
+    void draw(const World & world);
+    void stop();
+    SDL_Window * window;
+    SDL_Renderer * renderer;
+public:
+    int render(const World & world);
+};
+#endif //EVE_SDLVIEW_H
