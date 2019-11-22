c56aa7e7b080241801a8175abe41994deeb246cf
AtomToast <reg-2@t-online.de>
2019-11-22 18:49:23 +0100

    feat(view): create a visualisation of the individuals in the world

--- src/view/sdlview/sdlview.h
+++ src/view/sdlview/sdlview.h
@@ -3,9 +3,7 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
-#include <stdexcept>
 
-#include "../../config.h"
 #include "../../model/world/world.h"
 
 class SDLView {
@@ -13,11 +11,9 @@ private:
     void setupsdl(const World & world);
     void draw(const World & world);
     void stop();
-    SDL_Window * window{};
-    SDL_Renderer * renderer{};
-    SDL_DisplayMode DM;
+    SDL_Window * window;
+    SDL_Renderer * renderer;
 public:
-    [[nodiscard]]Point getRelativePosition(const Point & position, const double worldWidth, const double worldHeight, const int windowHeight, const int windowWidth) const;
     int render(const World & world);
 };
 #endif //EVE_SDLVIEW_H
