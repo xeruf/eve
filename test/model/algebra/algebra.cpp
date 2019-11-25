9b16fb65d73ba038a6e16f2a5e1e110b8602ded3
Aaron Alef <aaron.alef@code.berlin>
2019-11-25 12:19:54 +0100

    test(model): add assignment operator tests vector <=> point

--- test/model/algebra/algebra.cpp
+++ test/model/algebra/algebra.cpp
@@ -14,16 +14,21 @@ TEST_CASE("Points and Vectors can be used interchangeably") {
     double y = dist.rand();
     std::cout << "SEED: " << dist.SEED << std::endl;
 
-    Point p (x, y);
-    Vector vp (p);
-    Point pvp (vp);
-
     SECTION ("Points and Vectors can be created from each other") {
+        Point p (x, y);
+        Vector vp (p);
+        Point pvp (vp);
+
         CHECK (p == vp);
         CHECK (p == pvp);
         CHECK (vp == p);
         CHECK (vp == pvp);
         CHECK (pvp == p);
         CHECK (pvp == vp);
+
+        p = vp;
+        vp = pvp;
+        CHECK (p == vp);
+        CHECK (vp == pvp);
     }
 }
