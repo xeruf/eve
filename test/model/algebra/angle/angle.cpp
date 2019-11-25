3c30e91fb3e5dd8b617109c1dd292561c2c84c49
Aaron Alef <aaron.alef@code.berlin>
2019-11-25 14:24:26 +0100

    test(model): move outsourced getAngles() function back to angles test
    This reverts commit 8690b1be52e7378679bb7458672ce5fbfc9e765b.

--- test/model/algebra/angle/angle.cpp
+++ test/model/algebra/angle/angle.cpp
@@ -1,9 +1,9 @@
 #include "../../../../src/model/algebra/angle/angle.h"
-#include "../angle-vector.h"
 
 #include <catch2/catch.hpp>
 #include <vector>
 
+static std::vector<std::vector<Angle>> getAngles();
 
 TEST_CASE("Angles can substitute doubles while keeping between [0; 2 * PI)") {
     auto ANGLES = getAngles();
@@ -58,3 +58,44 @@ TEST_CASE("Angles can substitute doubles while keeping between [0; 2 * PI)") {
 }
 
 
+static std::vector<std::vector<Angle>> getAngles() {
+    // 0 degrees ANGLES
+    static std::vector<Angle> A;
+    A.emplace_back(0.0);
+    A.emplace_back(0);
+    A.emplace_back(-360);
+    A.emplace_back(360);
+    A.emplace_back(720);
+    A.emplace_back(2 * M_PI);
+
+
+    // 90 degrees ANGLES
+    static std::vector<Angle> B;
+    B.emplace_back(0.5 * M_PI);
+    B.emplace_back(90);
+    B.emplace_back(450);
+    B.emplace_back(-270);
+
+
+    // 180 degrees ANGLES
+    static std::vector<Angle> C;
+    C.emplace_back(M_PI);
+    C.emplace_back(180);
+    C.emplace_back(-180);
+
+
+    // 270 degrees ANGLES
+    static std::vector<Angle> D;
+    D.emplace_back(1.5 * M_PI);
+    D.emplace_back(270);
+    D.emplace_back(-90);
+
+
+    static std::vector<std::vector<Angle>> ANGLES;
+    ANGLES.push_back(A);
+    ANGLES.push_back(B);
+    ANGLES.push_back(C);
+    ANGLES.push_back(D);
+
+    return ANGLES;
+}
