06e43ec3d4d40d274d482ff3642d1629ec6b80ab
AtomToast <reg-2@t-online.de>
2019-11-26 13:44:34 +0100

    feat(sdlview): make the rander function return pressed keycodes

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -35,7 +35,8 @@ long Controller::run() {
 
 bool Controller::simulate() {
     if (RENDER_TERMINALVIEW) terminalview.render(world);
-    if(!sdlview.render(world)) return false;
+    int keysm = sdlview.render(world);
+    if (keysm == -1) return false;
     for (auto individual : world.getIndividuals()) {
 
         auto visibles = world.getObjectsAround(individual->getPosition(), individual->getVision().length);
