1905876861ec9729ed230581b76e10a1a870871b
AtomToast <reg-2@t-online.de>
2019-11-26 18:10:34 +0100

    feat: whut si dsijd

--- src/view/sdlview/sdlview.h
+++ src/view/sdlview/sdlview.h
@@ -1,8 +1,8 @@
 #ifndef EVE_SDLVIEW_H
 #define EVE_SDLVIEW_H
 
-#include <SDL.h>
-#include <SDL2_gfxPrimitives.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 #include <stdexcept>
 
 #include "../../config.h"
@@ -11,7 +11,6 @@
 class SDLView {
 private:
     void setupsdl(const World & world);
-    [[nodiscard]]Point getRelativePosition(const Point & position, const double worldWidth, const double worldHeight) const;
     void draw(const World & world);
     void stop();
     SDL_Window * window{};
