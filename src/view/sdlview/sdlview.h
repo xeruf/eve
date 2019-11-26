95be86be2387c18a655b8c4661dd5214c1ca798f
AtomToast <reg-2@t-online.de>
2019-11-26 18:35:29 +0100

    test: adasgsrthjstdz sth

--- src/view/sdlview/sdlview.h
+++ src/view/sdlview/sdlview.h
@@ -1,8 +1,8 @@
 #ifndef EVE_SDLVIEW_H
 #define EVE_SDLVIEW_H
 
-//#include <SDL2/SDL.h>
-//#include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 
 #include "../../config.h"
 #include "../../model/world/world.h"
