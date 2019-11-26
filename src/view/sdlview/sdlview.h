e3e8060043b2244bb1f88c15a8db3b13e6836ace
AtomToast <reg-2@t-online.de>
2019-11-26 15:46:44 +0100

    feat(sdlview): make SDL window dimensions of world

--- src/view/sdlview/sdlview.h
+++ src/view/sdlview/sdlview.h
@@ -4,15 +4,18 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 
+#include "../../config.h"
 #include "../../model/world/world.h"
 
 class SDLView {
 private:
     void setupsdl(const World & world);
+    [[nodiscard]]Point getRelativePosition(const Point & position, const double worldWidth, const double worldHeight) const;
     void draw(const World & world);
     void stop();
-    SDL_Window * window;
-    SDL_Renderer * renderer;
+    SDL_Window * window{};
+    SDL_Renderer * renderer{};
+    SDL_DisplayMode DM;
 public:
     int render(const World & world);
 };
