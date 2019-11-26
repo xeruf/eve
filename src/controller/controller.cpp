af03fb5301ec4480630152cddd5bf0ad3eade158
AtomToast <reg-2@t-online.de>
2019-11-26 18:07:12 +0100

    style: fix intendendation

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -34,8 +34,7 @@ long Controller::run() {
 }
 
 bool Controller::simulate() {
-    if (RENDER_TERMINALVIEW)
-        terminalview.render(world);
+    if (RENDER_TERMINALVIEW) terminalview.render(world);
     if (RENDER_SDLVIEW) {
         int keysm = sdlview.render(world);
         if (keysm == -1) return false;
@@ -48,7 +47,6 @@ bool Controller::simulate() {
         delete action;
         delete visibles;
     }
-    if(world.getIndividuals().size() > 0)
-        world.kill(world.getIndividuals().front()->getID());
+    if(world.getIndividuals().size() > 0) world.kill(world.getIndividuals().front()->getID());
     return RENDER_SDLVIEW || not world.getIndividuals().empty();
 }
