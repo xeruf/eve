c825911f54c640a78e14d30d908860251bc7e21b
Aaron Alef <aaron.alef@code.berlin>
2019-11-29 13:16:27 +0100

    feat(controller): empower individuals to move based on their action

--- src/model/algebra/vector/vector.cpp
+++ src/model/algebra/vector/vector.cpp
@@ -29,6 +29,10 @@ Vector & Vector::operator -= (const Angle & a) {
 }
 Vector & Vector::operator -= (const Vector & v) {return * this = * this - v;}
 
+Vector & Vector::operator += (const Angle & a) {
+    this->angle += a;
+    return * this;
+}
 Vector & Vector::operator += (const Vector & v) {return * this = * this + v;}
 
 bool   Vector::operator == (const Angle & a)  const {return a == * this;}
