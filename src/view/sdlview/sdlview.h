33d54ee11fd986e115103ffb5319a034944e9cc5
AtomToast <reg-2@t-online.de>
2019-11-30 15:58:48 +0100

    test: fix tests for terminalview and add tests for sdlview
    terminalview:
    changed how individuals are added to the world
    
    sdlview:
    added tests for getRelativePosition
    made getRelativePosition take windowWidth and Height as parameters
    made getRelativePosition public

--- src/view/sdlview/sdlview.h
+++ src/view/sdlview/sdlview.h
@@ -10,13 +10,13 @@
 class SDLView {
 private:
     void setupsdl(const World & world);
-    [[nodiscard]]Point getRelativePosition(const Point & position, const double worldWidth, const double worldHeight) const;
     void draw(const World & world);
     void stop();
     SDL_Window * window{};
     SDL_Renderer * renderer{};
     SDL_DisplayMode DM;
 public:
+    [[nodiscard]]Point getRelativePosition(const Point & position, const double worldWidth, const double worldHeight, const int windowHeight, const int windowWidth) const;
     int render(const World & world);
 };
 #endif //EVE_SDLVIEW_H
