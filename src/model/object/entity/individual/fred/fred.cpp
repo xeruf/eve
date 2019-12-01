a0600e77edf1bd25ea0c91ee4df7a0c0d016974e
Aaron Alef <aaron.alef@code.berlin>
2019-12-01 20:31:49 +0100

    feat(object): add random behaviour to Freds

--- src/model/object/entity/individual/fred/fred.cpp
+++ src/model/object/entity/individual/fred/fred.cpp
@@ -7,6 +7,5 @@ Fred::Fred(long ID, Point position, double a, double energy) :
     Individual(ID, position, a, energy) {}
 
 Action Fred::act(const std::unique_ptr<std::vector<Object *>> & visibles) {
-//    return Action((action) floor(Uniform(0, 3).rand()));
-return Action(MOVE);
+    return Action((action) floor(Uniform(0, 3).rand()));
 }
