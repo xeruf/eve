e7f2386a937687096286116a48729f6635c250f3
Aaron Alef <aaron.alef@code.berlin>
2019-12-02 19:47:28 +0100

    fix(controller): have them actually eat their food

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -116,6 +116,7 @@ void Controller::eatNearbyFood (Individual & individual) {
             individual.getRadius() + sqrt(MAX_FOOD_SIZE) / M_PI);
 
     for (auto & food : Foods) {
+        individual.eat(food);
         world.remove (& food);
     }
 }
