755fb95c2ca4584daeb46bdcb8ee36afd412e682
Aaron Alef <aaron.alef@code.berlin>
2019-12-03 15:21:40 +0100

    style(controller): format parentheses uniformly
    * Add spaces before filled parantheses (.).
    * Add spaces before filled pointy brackets <.>

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -1,8 +1,7 @@
 #include "controller.h"
 
-Controller::Controller(double WIDTH, double HEIGHT, double ENERGY) :
-    world(World(WIDTH, HEIGHT, ENERGY)) {
-}
+Controller::Controller (double WIDTH, double HEIGHT, double ENERGY) :
+    world (World (WIDTH, HEIGHT, ENERGY)) {}
 
 void Controller::init() {
     {
@@ -19,25 +18,25 @@ void Controller::init() {
                 for (counter = 0; counter < amounts[i]; counter++) {
                     switch (i) {
                         case 0:
-                            world.addIndividual<Fred>(
-                                    world.rand(X_d),
-                                    world.rand(Y_d),
-                                    world.rand(DIRECTION_d),
-                                    world.rand(ENERGY_d) * INDIVIDUAL_FOOD_FACTOR);
+                            world.addIndividual <Fred> (
+                                    world.rand (X_d),
+                                    world.rand (Y_d),
+                                    world.rand (DIRECTION_d),
+                                    world.rand (ENERGY_d) * INDIVIDUAL_FOOD_FACTOR);
                             break;
                         case 1:
-                            world.addIndividual<Brandy>(
-                                    world.rand(X_d),
-                                    world.rand(Y_d),
-                                    world.rand(DIRECTION_d),
-                                    world.rand(ENERGY_d) * INDIVIDUAL_FOOD_FACTOR);
+                            world.addIndividual <Brandy> (
+                                    world.rand (X_d),
+                                    world.rand (Y_d),
+                                    world.rand (DIRECTION_d),
+                                    world.rand (ENERGY_d) * INDIVIDUAL_FOOD_FACTOR);
                             break;
                         case 2:
-                            world.addIndividual<Piercy>(
-                                    world.rand(X_d),
-                                    world.rand(Y_d),
-                                    world.rand(DIRECTION_d),
-                                    world.rand(ENERGY_d) * INDIVIDUAL_FOOD_FACTOR);
+                            world.addIndividual <Piercy> (
+                                    world.rand (X_d),
+                                    world.rand (Y_d),
+                                    world.rand (DIRECTION_d),
+                                    world.rand (ENERGY_d) * INDIVIDUAL_FOOD_FACTOR);
                             break;
                     }
                 }
@@ -48,30 +47,30 @@ void Controller::init() {
             std::cout << counter << std::endl;
         }
     }
-    world.setRefillFunction([](World * w) -> Food * {
+    world.setRefillFunction([] (World * w) -> Food * {
         return new Food(
-                w->rand(X_d),
-                w->rand(Y_d),
+                w->rand (X_d),
+                w->rand (Y_d),
                 (w->ENERGY - w->getEnergy()) < MAX_FOOD_SIZE ?
                 w->ENERGY - w->getEnergy() :
-                w->rand(ENERGY_d));
+                w->rand (ENERGY_d));
     });
 
     initialised = true;
 }
 
 long Controller::run() {
-    if (not initialised) throw(std::logic_error("Controller::run(): .init() must be run first"));
+    if (not initialised) throw (std::logic_error ("Controller::run(): .init() must be run first"));
     while (simulate()) iteration++;
     return iteration;
 }
 
 bool Controller::simulate() {
-    world.fillWithFood();
+    world.fillWithFood( );
 
     if (RENDER_TERMINALVIEW) terminalview.render(world);
     if (RENDER_SDLVIEW) {
-        int keysm = sdlview.render(world);
+        int keysm = sdlview.render (world);
         if (keysm == -1) return false;
     }
 
@@ -80,23 +79,23 @@ bool Controller::simulate() {
         update (* individual, action);
     }
 
-    return not world.getIndividuals().empty();
+    return not world.getIndividuals().empty ();
 }
 
 action Controller::applyAction (Individual & individual) {
-    auto visibles = world.getObjectsInCone(individual.getPosition(), individual.getVision(), Angle(MOUTH_ANGLE));
+    auto visibles = world.getObjectsInCone (individual.getPosition(), individual.getVision (), Angle (MOUTH_ANGLE));
     action action = individual.act(visibles).type;
     switch (action) {
         case SLEEP:
             break;
         case MOVE:
-            individual.applyForce(Vector(individual.getVision().angle, MOVE_RATE + ACTION_FACTOR_MOVE * individual.getEnergy()));
+            individual.applyForce (Vector (individual.getVision().angle, MOVE_RATE + ACTION_FACTOR_MOVE * individual.getEnergy ()));
             break;
         case TURN_LEFT:
-            individual.turnBy(Angle(- (int) (TURN_RATE + ACTION_FACTOR_TURN * individual.getEnergy())));
+            individual.turnBy (Angle(- (int) (TURN_RATE + ACTION_FACTOR_TURN * individual.getEnergy())));
             break;
         case TURN_RIGHT:
-            individual.turnBy(Angle((int) (TURN_RATE + ACTION_FACTOR_TURN * individual.getEnergy())));
+            individual.turnBy (Angle((int) (TURN_RATE + ACTION_FACTOR_TURN * individual.getEnergy())));
             break;
     }
     return action;
@@ -104,7 +103,7 @@ action Controller::applyAction (Individual & individual) {
 
 void Controller::update (Individual & individual, action action) {
     individual.applyFriction();
-    individual.updatePosition(std::bind(& World::normalisePosition, & world, std::placeholders::_1));
+    individual.updatePosition (std::bind (& World::normalisePosition, & world, std::placeholders::_1));
     updateEnergy (individual, action);
     eatNearbyFood (individual);
 }
@@ -129,17 +128,17 @@ void Controller::updateEnergy (Individual & individual, action action) {
         world.kill (individual.getID());
     }
     if (energyLevel > REPRODUCTION_THRESHOLD) {
-        world.addChild (individual.reproduce(world.getIndividuals().size()));
+        world.addChild (individual.reproduce(world.getIndividuals().size ()));
     }
 }
 
 void Controller::eatNearbyFood (Individual & individual) {
-    std::vector<Food *> Foods = * world.getFoodsAround(
+    std::vector <Food *> Foods = * world.getFoodsAround (
             individual.getPosition(),
-            individual.getRadius() + sqrt(MAX_FOOD_SIZE) / M_PI);
+            individual.getRadius() + sqrt (MAX_FOOD_SIZE) / M_PI);
 
     for (auto food : Foods) {
-        individual.eat(* food);
+        individual.eat (* food);
         world.remove (food);
     }
 }
