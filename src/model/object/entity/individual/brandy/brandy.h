207ea83b725584b03d4c9690102039f012eb11c2
Aaron Alef <aaron.alef@code.berlin>
2019-12-03 13:31:34 +0100

    feat(object): add new species, Brandy; it has random behavior
    Also add `override` kw to the reproduce() method

--- src/model/object/entity/individual/brandy/brandy.h
+++ src/model/object/entity/individual/brandy/brandy.h
@@ -1,38 +1,29 @@
-#ifndef EVE_FRED_H
-#define EVE_FRED_H
+#ifndef EVE_BRANDY_H
+#define EVE_BRANDY_H
 
 #include "../individual.h"
 #include "../../../object.h"
 #include "../../../../action/action.h"
 #include "../../../../random/random.h"
 
-#include <unordered_map>
-
-/** NPC Species supposedly acting *smart**/
-class Fred : public Individual {
-private:
-    action previousActions;
-    double previousEnergy;
-    std::unordered_map<action, double> memory;
-
-    action think(const std::unique_ptr<std::vector<Object *>> & visibles);
+class Brandy : public Individual {
 public:
-    /** Constructs a new individual of species Fred
+    /** Constructs a new individual of species Species
      * @param ID is unique and set automatically by the World
      * @param x is the latitude of the object
      * @param y is the longitude of the object
      * @param a is the direction it's looking
      * @param energy is its life source */
-    Fred(long ID, double x, double y, double a, double energy);
-    /** Constructs a new individual of species Fred
+    Brandy(long ID, double x, double y, double a, double energy);
+    /** Constructs a new individual of species Species
      * @param ID is unique and set automatically by the World
-     * @param position sets the coordinates of Fred
+     * @param position sets the coordinates of Species
      * @param a is the direction it's looking
      * @param energy is its life source */
-    Fred(long ID, Point position, double a, double energy);
+    Brandy(long ID, Point position, double a, double energy);
 
     /** Methods giving birth to an offspring */
-    Fred * reproduce (long ID) override;
+    Brandy * reproduce (long ID) override;
 
     /** Returns an Action using Neural Networks, and Machine, Deep and Reinforcement Learning
      * @param visibles is a pointer to an array holding all objects within its vision
@@ -40,4 +31,5 @@ public:
     Action act(const std::unique_ptr<std::vector<Object *>> & visibles) override;
 };
 
-#endif //EVE_FRED_H
+
+#endif //EVE_BRANDY_H
