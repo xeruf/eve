dce5871530316503816967aca6326e471b5177f5
Aaron Alef <aaron.alef@code.berlin>
2019-12-03 13:25:12 +0100

    feat(model): have individuals reproduce

--- src/model/object/entity/individual/piercy/piercy.cpp
+++ src/model/object/entity/individual/piercy/piercy.cpp
@@ -6,6 +6,12 @@ Piercy::Piercy(long ID, double x, double y, double a, double energy) :
 Piercy::Piercy(long ID, Point position, double a, double energy) :
     Individual(ID, position, a, energy) {}
 
+Piercy * Piercy::reproduce(long ID) {
+    double energy = getEnergy();
+    updateEnergy(1 - REPRODUCTION_CHILD_FRACTION);
+    return new Piercy (ID, position, getVision().angle.radians(), REPRODUCTION_CHILD_FRACTION * energy);
+}
+
 Action Piercy::act(const std::unique_ptr<std::vector<Object *>> & visibles) {
     while (true) {
         std::cin.ignore(FLUSH_AMOUNT, '\n');
