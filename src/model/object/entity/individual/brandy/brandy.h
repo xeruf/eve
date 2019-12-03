ad36d7450d5572634bcc258eb3fc825ab13a5a7b
Aaron Alef <aaron.alef@code.berlin>
2019-12-03 15:26:15 +0100

    docs(object): change 'Species' to the actual species in the docs

--- src/model/object/entity/individual/brandy/brandy.h
+++ src/model/object/entity/individual/brandy/brandy.h
@@ -9,16 +9,16 @@
 /** Non-Player-controlled Species performing completely random actions */
 class Brandy : public Individual {
 public:
-    /** Constructs a new individual of species Species
+    /** Constructs a new individual of species Brandy
      * @param ID is unique and set automatically by the World
      * @param x is the latitude of the object
      * @param y is the longitude of the object
      * @param a is the direction it's looking
      * @param energy is its life source */
     Brandy(long ID, double x, double y, double a, double energy);
-    /** Constructs a new individual of species Species
+    /** Constructs a new individual of species Brandy
      * @param ID is unique and set automatically by the World
-     * @param position sets the coordinates of Species
+     * @param position sets the coordinates of Brandy
      * @param a is the direction it's looking
      * @param energy is its life source */
     Brandy(long ID, Point position, double a, double energy);
