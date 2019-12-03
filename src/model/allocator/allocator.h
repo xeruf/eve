f99f545ee6dc16f42113f587221fef0e5b247a65
Aaron Alef <aaron.alef@code.berlin>
2019-12-03 15:01:02 +0100

    docs(model): add documentation to the allocator

--- src/model/allocator/allocator.h
+++ src/model/allocator/allocator.h
@@ -8,6 +8,7 @@
 
 #include <stdlib.h>
 
+/** Class used for allocating memory for objects which are within a confined space, thus improving performance */
 template <class T>
 class Allocator {
 private:
@@ -45,6 +46,7 @@ public:
         free (objects);
     }
 
+    /** Returns a pointer to one or 'n' objects of the templated type */
     T * allocate (size_t n = 1) {
         if (n <= 0) throw std::range_error("Allocator::allocate: Can't allocate non-zero amounts of objects");
         if (n >= PREALLOCATED_OBJECTS) throw std::bad_alloc();
@@ -62,6 +64,9 @@ public:
         throw std::bad_alloc();
     }
 
+    /** Frees space of a previously allocated memory
+     * @param ptr should point to memory previously allocated from this object
+     * @param n should have the same size as how many objects got allocated */
     void deallocate (T * ptr, size_t n = 1) {
         auto item = allocs.find(ptr);
         if (item != allocs.end()) {
